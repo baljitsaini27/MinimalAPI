a7e8df36280c0b8cf5417db79180281a415f1466
baljitsaini27 <40536068+baljitsaini27@users.noreply.github.com>
2024-07-22 13:46:09 +0530

    Add files via upload

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,1 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
 
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/todoitems", async () =>
+{
+    var list = new List<Todo>();
+    var item1 = new Todo()
+    {
+        Id = 1,
+        Name ="test",
+        IsComplete = true
+    };
+
+    list.Add(item1);
+    var item2 = new Todo()
+    {
+        Id = 2,
+        Name = "abc",
+        IsComplete = false
+    };
+    list.Add(item2);
+
+    return list;
+});
+
+app.MapGet("/todoitems/{id}", async (int id) =>
+{
+    var list = new List<Todo>();
+    var item1 = new Todo()
+    {
+        Id = 1,
+        Name = "test",
+        IsComplete = true
+    };
+
+    list.Add(item1);
+    var item2 = new Todo()
+    {
+        Id = 2,
+        Name = "abc",
+        IsComplete = false
+    };
+    list.Add(item2);
+
+    return list.FirstOrDefault(x => x.Id == id) is Todo todo ? Results.Ok(todo) : Results.NotFound();
+
+});
+
+app.Run();
